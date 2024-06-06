def988009dad02786d357186ec57c0c12e821631
eak <eak@ivalua.com>
2024-06-06 17:49:58 +0000

    Starting project with finetuning of phi-3

---
+++ installs.sh
@@ -0,0 +1,5 @@
+pip install torch torchvision torchaudio xformers --index-url https://download.pytorch.org/whl/cu118
+pip install transformers[torch]
+pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
+pip install --no-deps trl peft accelerate bitsandbytes
+pip install matplotlib pickleshare pandas numpy python-docx
