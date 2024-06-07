9f543af22aef2f0bf419855806665224725fd2d1
eak <eak@ivalua.com>
2024-06-07 00:03:32 +0000

    Add inference notebook

--- train_model.py
+++ train_model.py
@@ -11,6 +11,9 @@ os.environ["WANDB_LOG_MODEL"] = "true"
 os.environ["WANDB_WATCH"] = "none"
 os.environ["WANDB_NOTEBOOK_NAME"] = "qa_telcom"
 
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
 import json
 
 # %%
@@ -20,33 +23,42 @@ from trl import SFTTrainer
 from transformers import TrainingArguments
 
 # %%
-max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
-dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
-load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+max_seq_length = 2048  # Choose any! We auto support RoPE Scaling internally!
+dtype = (
+    None  # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+)
+load_in_4bit = True  # Use 4bit quantization to reduce memory usage. Can be False.
 
 model_name = "unsloth/Phi-3-mini-4k-instruct"
 model, tokenizer = FastLanguageModel.from_pretrained(
-	model_name = model_name,
-	max_seq_length = max_seq_length,
-	dtype = dtype,
-	load_in_4bit = load_in_4bit,
-	# token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+    model_name=model_name,
+    max_seq_length=max_seq_length,
+    dtype=dtype,
+    load_in_4bit=load_in_4bit,
+    # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
 )
 
 # %%
 model = FastLanguageModel.get_peft_model(
-	model,
-	r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
-	target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
-					  "gate_proj", "up_proj", "down_proj",],
-	lora_alpha = 16,
-	lora_dropout = 0, # Supports any, but = 0 is optimized
-	bias = "none",    # Supports any, but = "none" is optimized
-	# [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
-	use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
-	random_state = 3407,
-	use_rslora = False,  # We support rank stabilized LoRA
-	loftq_config = None, # And LoftQ
+    model,
+    r=16,  # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "gate_proj",
+        "up_proj",
+        "down_proj",
+    ],
+    lora_alpha=16,
+    lora_dropout=0,  # Supports any, but = 0 is optimized
+    bias="none",  # Supports any, but = "none" is optimized
+    # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+    use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
+    random_state=3407,
+    use_rslora=False,  # We support rank stabilized LoRA
+    loftq_config=None,  # And LoftQ
 )
 
 # %%
@@ -55,16 +67,17 @@ print(model)
 # %%
 from datasets import Dataset
 
+
 def create_dataset(data: dict):
-	def patch_raw(raw: dict):
-		for i in range(2, 6):
-			raw[f"option {i}"] = raw.get(f"option {i}")
-		return raw
-	data_pashed = [
-		patch_raw(raw) for raw in data.values()
-	]
-	data_pashed = Dataset.from_list(data_pashed)
-	return data_pashed
+    def patch_raw(raw: dict):
+        for i in range(2, 6):
+            raw[f"option {i}"] = raw.get(f"option {i}")
+        return raw
+
+    data_pashed = [patch_raw(raw) for raw in data.values()]
+    data_pashed = Dataset.from_list(data_pashed)
+    return data_pashed
+
 
 # %%
 training = json.load(open("data/zindi_data/TeleQnA_training.json"))
@@ -97,17 +110,39 @@ alpaca_prompt = """Below is an instruction that describes a task, paired with an
 {}"""
 
 OPTIONS = [f"option {i}" for i in range(1, 6)]
-EOS_TOKEN = tokenizer.eos_token # Must add EOS_TOKEN
+EOS_TOKEN = tokenizer.eos_token  # Must add EOS_TOKEN
+
+
 def formatting_prompts_func(examples: dict[str, str]):
-	def apply_one(question, answer, category, *options):
-		instructions = f"Domain: {category}: {question}"
-		inputs       = "\n".join([i for i in options if i is not None])
-		outputs      = answer
-		return alpaca_prompt.format(instructions, inputs, outputs)
-	texts = [apply_one(question, answer, category, *options) for question, answer, category, *options in zip(
-		examples["question"], examples["answer"],  examples['option 1'], examples['option 2'], examples['option 3'], examples['option 4'], examples['option 5']
-	)]
-	return { "text" : texts, }
+    def apply_one(question, answer, category, *options):
+        instructions = f"Domain: {category}: {question}"
+        inputs = "\n".join(
+            [
+                (f"option {i}: " + text)
+                for i, text in enumerate(options, start=1)
+                if text is not None
+            ]
+        )
+        outputs = answer
+        return alpaca_prompt.format(instructions, inputs, outputs)
+
+    texts = [
+        apply_one(question, answer, category, *options)
+        for question, answer, category, *options in zip(
+            examples["question"],
+            examples["answer"],
+            examples["category"],
+            examples["option 1"],
+            examples["option 2"],
+            examples["option 3"],
+            examples["option 4"],
+            examples["option 5"],
+        )
+    ]
+    return {
+        "text": texts,
+    }
+
 
 # %%
 training_ds = training_ds.map(formatting_prompts_func, batched=True)
@@ -124,43 +159,43 @@ print(extra_ds[:5]["text"])
 
 # %%
 trainer = SFTTrainer(
-	model = model,
-	tokenizer = tokenizer,
-	train_dataset = extra_ds,
-	eval_dataset = training_ds,
-	dataset_text_field = "text",
-	max_seq_length = max_seq_length,
-	dataset_num_proc = 2,
-	packing = False, # Can make training 5x faster for short sequences.
-	args = TrainingArguments(
+    model=model,
+    tokenizer=tokenizer,
+    train_dataset=extra_ds,
+    eval_dataset=training_ds,
+    dataset_text_field="text",
+    max_seq_length=max_seq_length,
+    dataset_num_proc=2,
+    packing=False,  # Can make training 5x faster for short sequences.
+    args=TrainingArguments(
         output_dir="data/models",
         run_name="qa_telcom",
-		per_device_train_batch_size = 2,
-		gradient_accumulation_steps = 4,
-		warmup_ratio = 0.05,
-		learning_rate = 2e-4,
-		fp16 = not torch.cuda.is_bf16_supported(),
-		bf16 = torch.cuda.is_bf16_supported(),
-		optim = "adamw_8bit",
-		weight_decay = 0.01,
-		lr_scheduler_type = "cosine",
-		seed = 3407,
-		push_to_hub=False,
-		auto_find_batch_size=True,
-		num_train_epochs=3,
-		save_total_limit=1,
-		eval_strategy="epoch",
-		save_strategy="epoch",
-		report_to="wandb",
-		data_seed=41,
-		load_best_model_at_end=True,
-		metric_for_best_model="loss",
-		save_only_model=True
-	),
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=4,
+        warmup_ratio=0.05,
+        learning_rate=2e-4,
+        fp16=not torch.cuda.is_bf16_supported(),
+        bf16=torch.cuda.is_bf16_supported(),
+        optim="adamw_8bit",
+        weight_decay=0.01,
+        lr_scheduler_type="cosine",
+        seed=3407,
+        push_to_hub=False,
+        auto_find_batch_size=True,
+        num_train_epochs=3,
+        save_total_limit=1,
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        report_to="wandb",
+        data_seed=41,
+        load_best_model_at_end=True,
+        metric_for_best_model="loss",
+        save_only_model=True,
+    ),
 )
 
 # %%
-#@title Show current memory stats
+# @title Show current memory stats
 gpu_stats = torch.cuda.get_device_properties(0)
 start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
 max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
@@ -184,6 +219,3 @@ tokenizer.save_pretrained("data/models/model1")
 trainer.evaluate()
 
 # %%
-
-
-
